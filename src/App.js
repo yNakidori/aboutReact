ed03b78ea896d780031ec5e8cdfb1580fee52511
Naki <leandroreisubi@gmail.com>
2024-06-07 17:19:49 -0300

    about React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const content = [
+  [
+    "React is extrmerly popular",
+    "It makes buildinf complex, interactive UIs a breeze",
+    "It's powerful & flexible",
+    "It has a very active and versatile ecossytem"
+  ],
+  [
+    "Components, JSX & Props",
+    "State",
+    "Hooks (e.g., useEffect()) ",
+    "Dynamic rendering"
+  ],
+  [
+    "Official web page (react.dev)",
+    "Next.js (Fullstack framework)",
+    "React Native (build native mobile apps with React"
+  ],
+  [
+    "Vanilla JavaScript requires imperative programing",
+    "Imperative Programming: You define all the steps needed to achieve a result",
+    "React on the other hand embraces declarative programming",
+    "With React, you define the goal and React figures out how to get there"
+  ]
+];
 function App() {
+  const [activeContentIndex, setActiveContentIndex] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +45,33 @@ function App() {
         >
           Learn React
         </a>
+        <div id='tabs'>
+          <menu>
+            <button className={activeContentIndex === 0 ? "active" : "active"}
+              onClick={() => setActiveContentIndex(0)}>
+              Why React?
+            </button>
+            <button className={activeContentIndex === 1 ? "active" : "active"}
+              onClick={() => setActiveContentIndex(1)}>
+              Core Concepts
+            </button>
+            <button className={activeContentIndex === 2 ? "active" : "active"}
+              onClick={() => setActiveContentIndex(2)}>
+              Ecosystem
+            </button>
+            <button className={activeContentIndex === 3 ? "active" : "active"}
+              onClick={() => setActiveContentIndex(3)}>
+              React vs Vanilla JS
+            </button>
+          </menu>
+          <div id='tab-content'>
+            <ul>
+              {content[activeContentIndex].map((item) => (
+                <li key={item}>{item}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </header>
     </div>
   );
